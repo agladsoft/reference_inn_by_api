2179f52de5d25fa1aae26de46e9dda8f6a8717c7
timur <timurka.zavyalov@mail.ru>
2023-10-18 13:12:09 +0300

    add: added THREAD in scripts instead of MULTIPROCESSING

--- get_difference.py
+++ get_difference.py
@@ -1,14 +1,21 @@
 import pandas as pd
-import numpy as np
 
 root_dir = '/home/timur/Загрузки'
+file_path_from_db = f'{root_dir}/select company_name, company_inn(1).xlsx'
+file_path_from_marketing = f'{root_dir}/reference_inn_all_production.xlsx'
 
-file_path_from_db = f'{root_dir}/select _ from reference_inn wher (3).xlsx'
-file_path_from_marketing = f'{root_dir}/Test_inn.xlsx'
+file_from_db = pd.read_excel(file_path_from_db).sort_values(by=['company_name'], ignore_index=True)
+file_from_marketing = pd.read_excel(file_path_from_marketing).sort_values(by=['company_name'], ignore_index=True)
 
-file_from_db = pd.read_excel(file_path_from_db)
-file_from_marketing = pd.read_excel(file_path_from_marketing)
+df_difference = pd.DataFrame()
 
-np_difference = np.setdiff1d(file_from_marketing['company_name'], file_from_db['company_name'])
-df_difference = pd.DataFrame(data=np_difference)
-df_difference.to_csv(f'{root_dir}/difference.csv', index=False)
+index_db: int = 0
+for index, row in file_from_marketing.iterrows():
+    value: str = file_from_db.loc[index_db, "company_name"]
+    if row["company_name"] == value:
+        index_db += 1
+    else:
+        df_difference = df_difference.append(row, ignore_index=True)
+        # df_difference = pd.concat([df_difference, row], ignore_index=True)
+
+df_difference.to_excel(f'{root_dir}/difference.xlsx', index=False)
