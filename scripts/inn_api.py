d042d00c3a81f9a91d339a599bcc1884abf0de75
timur <timurka.zavyalov@mail.ru>
2023-11-09 12:55:03 +0300

    add: added .env

--- scripts/inn_api.py
+++ scripts/inn_api.py
@@ -8,8 +8,7 @@ from typing import Union, Tuple
 from threading import current_thread
 from requests_html import HTMLSession
 import xml.etree.ElementTree as ElemTree
-from __init__ import logger, logger_stream, USER_XML_RIVER, KEY_XML_RIVER, MESSAGE_TEMPLATE, PREFIX_TEMPLATE, \
-    get_my_env_var
+from __init__ import logger, logger_stream, USER_XML_RIVER, KEY_XML_RIVER, MESSAGE_TEMPLATE, PREFIX_TEMPLATE
 
 
 class MyError(Exception):
